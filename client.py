397e783a013b64889fb6d0d604e4a75c88186537
fahim-kamal <fkamal@usc.edu>
2023-05-05 13:09:27 -0700

    changed ui 5

--- client.py
+++ client.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
           # Start LED to indicate recording in process
           digitalWrite(RED_LED_PIN, 1)
           setRGB(255,0,0)
+          setText("Recording...")
 
           record(checkButton, BUTTON_PIN)
 
