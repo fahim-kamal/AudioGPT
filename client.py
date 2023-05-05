af7694b84134ca9eb48fdfd3ab749a4659a54d30
fahim-kamal <fkamal@usc.edu>
2023-05-05 10:09:47 -0700

    renamed some files

--- client.py
+++ client.py
@@ -12,21 +12,25 @@ state = States.STARTUP
 STARTUP_MSG = "Welcome to AudioGPT, an audio interface for accessing ChatGPT!"
 
 if __name__ == "__main__":
-  while True:
-    if state == States.STARTUP:
-      setup()
-      scrollText(STARTUP_MSG)
+  try:
+    while True:
+      if state == States.STARTUP:
+        setup()
+        scrollText(STARTUP_MSG)
 
-      state = States.IDLE
-    elif state == States.IDLE:
-      BUTTON_STATE = digitalRead(BUTTON_PIN)
+        state = States.IDLE
+      elif state == States.IDLE:
+        BUTTON_STATE = digitalRead(BUTTON_PIN)
 
-      if (BUTTON_STATE):
-        # Start LED to indicate recording in process
-        digitalWrite(RED_LED_PIN, 1)
+        if (BUTTON_STATE):
+          # Start LED to indicate recording in process
+          digitalWrite(RED_LED_PIN, 1)
+
+          
 
 
 
-        scrollText("You have clicked the button!")
-
+          scrollText("You have clicked the button!")
+  except KeyboardInterrupt:
+    print("\n" + "Service ended.")
 
---
+++ recording.py
@@ -0,0 +1,21 @@
+import sounddevice as sd
+import soundfile as sf
+import queue
+
+fs = 44100  # Sample rate
+MICROPHONE = "USB PnP Sound Device"
+
+q = queue.Queue()
+
+def callback(indata, frames, time, status):
+    q.put(indata.copy())
+
+try:
+    with sf.SoundFile("output.wav", mode='x', samplerate=fs, channels=2) as file:
+        with sd.InputStream(samplerate=fs, device=MICROPHONE, channels=2, callback=callback):
+            while True:
+                file.write(q.get())
+
+except KeyboardInterrupt:
+    print("Recording Finished")
+
