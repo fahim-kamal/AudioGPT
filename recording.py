e935e4f426b3e5833014f768c327cb80ad8bf67d
fahim-kamal <fkamal@usc.edu>
2023-05-05 10:10:59 -0700

    changed num channels

--- recording.py
+++ recording.py
@@ -11,8 +11,8 @@ def callback(indata, frames, time, status):
     q.put(indata.copy())
 
 try:
-    with sf.SoundFile("output.wav", mode='x', samplerate=fs, channels=2) as file:
-        with sd.InputStream(samplerate=fs, device=MICROPHONE, channels=2, callback=callback):
+    with sf.SoundFile("output.wav", mode='x', samplerate=fs, channels=1) as file:
+        with sd.InputStream(samplerate=fs, device=MICROPHONE, channels=1, callback=callback):
             while True:
                 file.write(q.get())
 
