324383b05d5a5d9fcecc3b83298395dda5e69a39
fahim-kamal <fkamal@usc.edu>
2023-05-05 10:14:05 -0700

    added auto delete functionality for recording

--- recording.py
+++ recording.py
@@ -1,16 +1,23 @@
 import sounddevice as sd
 import soundfile as sf
 import queue
+import os
 
 fs = 44100  # Sample rate
 MICROPHONE = "USB PnP Sound Device"
 
 q = queue.Queue()
+path = "./output.wav"
 
 def callback(indata, frames, time, status):
     q.put(indata.copy())
 
 try:
+    doesFileExist = os.path.isfile(path)
+
+    if doesFileExist:
+        os.remove(path)
+
     with sf.SoundFile("output.wav", mode='x', samplerate=fs, channels=1) as file:
         with sd.InputStream(samplerate=fs, device=MICROPHONE, channels=1, callback=callback):
             while True:
